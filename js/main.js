2254c36ad83ba3739a7faf5581995b77f51d4e9d
djidji3 <djidji@freemail.hu>
2020-12-27 15:57:32 +0100

    css modify

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ function loadTodo2PendingDiv(todo) {
     todoItem.innerHTML = `
     <input class='' type="checkbox" name="" id="">
     <span class='task'>${todo.task}</span>
-    <button data-id=${todo.id} class='todo__list-deleteBtn'>Törlés</button>
+    <button data-id=${todo.id} class='todo__list-deleteBtn'>&#10006;</button>
     `;
     newTaskInput.value = '';
 };
@@ -154,7 +154,7 @@ function loadTodo2DoneDiv(todo) {
     todoItem.setAttribute('data-id', `${todo.id}`);
     todoListDone.appendChild(todoItem);
     todoItem.innerHTML = `
-    <input class='checkBox-doneItem' type="checkbox" name="" id="">
+    <input class='checkBox-doneItem' type="checkbox" name="" id="" >
     <span class='task'>${todo.task}</span>
     `;
 };
